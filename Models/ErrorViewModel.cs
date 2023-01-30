44be96786eee8da3177bbba7e7b6580787a6ec22
ccs <ccs@CCSIPKCU8030>
2023-01-30 10:20:56 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CCS_Demo_Architect.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
